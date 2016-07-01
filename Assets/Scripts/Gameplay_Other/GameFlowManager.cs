2b564163079c3a37909bfc26be855c5967995cde
Diogo Delgado Barros <finkry@gmail.com>
2016-07-01 15:09:14 +0300

    Added more text features, started creating the game flow

---
+++ Assets/Scripts/Gameplay_Other/GameFlowManager.cs
@@ -0,0 +1,81 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameFlowManager : MonoBehaviour {
+
+    public string npcName;
+    public PlayerController player;
+    public ActivateTextAtLine textActivate;
+    public TextBoxManager textBoxManager;
+    public TextList textNumber;
+
+    public bool isIntro = true;
+    public bool destroyNPC;
+
+    public int npcIntroBehav = 0;
+    public int npc1Behav = 0;
+
+    public void DestroyNPC()
+    {
+        if (destroyNPC)
+        {
+            GameObject npcBeingDestroyed;
+            npcBeingDestroyed = GameObject.Find(npcName);
+            Destroy(npcBeingDestroyed);
+            destroyNPC = false;
+        }
+    }
+
+    public void ChangeNPCBehaviour()
+    {
+        if (npcName == "Intro_NPC")
+        {
+            if (npcIntroBehav == 0)
+            {
+                //darkScreen = false;
+                textNumber.text1StartLine = 3;
+                textNumber.text1EndLine = 4;
+                npcIntroBehav++;
+            }
+            else if (npcIntroBehav == 1)
+            {
+                destroyNPC = true;
+                npcIntroBehav++;
+            }
+            
+        }
+
+        else if (npcName == "NPC_1")
+        {
+            if (npc1Behav == 0)
+            {
+                textNumber.text1StartLine = 3;
+                textNumber.text1EndLine = 4;
+                npc1Behav++;
+            }
+            else if (npc1Behav == 1)
+            {
+                destroyNPC = true;
+                npc1Behav++;
+            }
+        }
+
+        DestroyNPC(); // destroys the npc if the bool is true
+    }
+
+	void Start () {
+        if (isIntro)
+        {
+            //darkScreen = true;
+        }
+
+        npcName = "";
+        textNumber = GameObject.Find("TextList").GetComponent<TextList>();
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        textBoxManager = GameObject.Find("TextBoxManager").GetComponent<TextBoxManager>();
+	}
+	
+	void Update () {
+        npcName = player.NPCName;
+	}
+}
---
+++ Assets/Scripts/Texts/npcBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class npcBehaviour : MonoBehaviour {
+
+    public PlayerController player;
+    public ActivateTextAtLine textLoader;
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            player.isOverlappingNPC = true;
+            if (other.gameObject.tag == "Player")
+            {
+                player.isSelectionActive = true;
+                textLoader.selection.SetActive(true);
+                player.selection = PlayerController.Selection.NPC;
+            }
+
+            player.playerAnim.SetBool("isIdle", true);
+            player.playerAnim.SetBool("isWalking", false);
+
+            if (textLoader.requireButtonPress)
+            {
+                textLoader.waitForPress = true;
+                return;
+            }
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            player.isOverlappingNPC = false;
+            textLoader.waitForPress = false;
+            player.selection = PlayerController.Selection.DEFAULT;
+            player.isSelectionActive = false;
+            textLoader.selection.SetActive(false);
+        }
+    }
+
+    void Awake () {
+        player = GameObject.Find("Player").GetComponent<PlayerController>();
+        textLoader = GameObject.Find("ActivateText").GetComponent<ActivateTextAtLine>();
+	}
+	
+	void Update () {
+	
+	}
+}

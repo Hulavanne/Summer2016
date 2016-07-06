f44ded5b42f0a339b9084faf6533ad7ad9ebd160
Diogo Delgado Barros <finkry@gmail.com>
2016-07-06 17:25:28 +0300

    started doing paralax

--- Assets/Scripts/Environment/BackgroundBehaviour.cs
+++ Assets/Scripts/Environment/BackgroundBehaviour.cs
@@ -3,19 +3,38 @@ using System.Collections;
 
 public class BackgroundBehaviour : MonoBehaviour {
 
+    public GameObject thisLevelPos;
     public GameObject player;
-    public GameObject mainCamera;
-    public float backgroundSpeed = 0.8f;
     public PlayerController playerControl;
-    
+    public GameObject mainCamera;
+    public float backgroundSpeed;
+    public LevelManager.Levels thisLevelValue;
+    public float playerInitPos;
+    public bool playerStart = true;
+
     void Start ()
     {
-        // jumps to player
-        transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y, transform.position.z);
+        mainCamera = GameObject.Find("MainCamera");
+        player = GameObject.Find("Player");
+        playerControl = player.GetComponent<PlayerController>();
     }
-	
-	void Update ()
+
+    void Update()
     {
-        transform.position = new Vector3(mainCamera.transform.position.x * backgroundSpeed, transform.position.y, transform.position.z);
+        Debug.Log("Working0");
+        if (playerControl.levelManager.currentLevel == thisLevelValue)
+        {
+            Debug.Log("Working1");
+
+            if (playerStart)
+            {
+                thisLevelPos.transform.position = new Vector3 (thisLevelPos.transform.position.x, thisLevelPos.transform.position.y, thisLevelPos.transform.position.z);
+                playerStart = false;
+            }
+
+            transform.position = new Vector3(thisLevelPos.transform.position.x + 
+            (player.transform.position.x - thisLevelPos.transform.position.x) * backgroundSpeed,
+            transform.position.y, transform.position.z);
+        }
     }
 }

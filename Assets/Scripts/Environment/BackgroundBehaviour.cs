6aa70f354c724fc8e6079b13f1361271c0ccc3fa
Diogo Delgado Barros <finkry@gmail.com>
2016-07-07 13:42:53 +0300

    more parallax

--- Assets/Scripts/Environment/BackgroundBehaviour.cs
+++ Assets/Scripts/Environment/BackgroundBehaviour.cs
@@ -1,7 +1,8 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class BackgroundBehaviour : MonoBehaviour {
+public class BackgroundBehaviour : MonoBehaviour
+{
 
     public GameObject thisLevelPos;
     public GameObject player;
@@ -9,32 +10,47 @@ public class BackgroundBehaviour : MonoBehaviour {
     public GameObject mainCamera;
     public float backgroundSpeed;
     public LevelManager.Levels thisLevelValue;
-    public float playerInitPos;
     public bool playerStart = true;
+    public float distanceFromStart;
+    public bool movesAlone;
+    public float moveAloneSpeed;
 
-    void Start ()
+    void Start()
     {
         mainCamera = GameObject.Find("MainCamera");
         player = GameObject.Find("Player");
         playerControl = player.GetComponent<PlayerController>();
+        moveAloneSpeed += 0.05f;
     }
 
     void Update()
     {
-        //Debug.Log("Working0");
         if (playerControl.levelManager.currentLevel == thisLevelValue)
         {
-            Debug.Log("Working1");
-
             if (playerStart)
             {
-                thisLevelPos.transform.position = new Vector3 (thisLevelPos.transform.position.x, thisLevelPos.transform.position.y, thisLevelPos.transform.position.z);
+                transform.position = new Vector3(thisLevelPos.transform.position.x, thisLevelPos.transform.position.y, thisLevelPos.transform.position.z);
                 playerStart = false;
+                distanceFromStart = thisLevelPos.transform.position.x - mainCamera.transform.position.x;
             }
-
-            transform.position = new Vector3(thisLevelPos.transform.position.x + 
-            (player.transform.position.x - thisLevelPos.transform.position.x) * backgroundSpeed,
-            transform.position.y, transform.position.z);
+            if (movesAlone)
+            {
+                transform.position = new Vector3(((mainCamera.transform.position.x
+                    + distanceFromStart) * backgroundSpeed) + moveAloneSpeed,
+                    transform.position.y, transform.position.z);
+                moveAloneSpeed += 0.2f * Time.deltaTime;
+            }
+            else
+            {
+                transform.position = new Vector3((mainCamera.transform.position.x
+                    + distanceFromStart) * backgroundSpeed,
+                    transform.position.y, transform.position.z);
+            }
+        }
+        else
+        {
+            playerStart = true;
+            moveAloneSpeed = 0;
         }
     }
 }
--- Assets/Scripts/Environment/ForegroundBehaviour.cs
+++ Assets/Scripts/Environment/ForegroundBehaviour.cs
@@ -3,21 +3,44 @@ using System.Collections;
 
 public class ForegroundBehaviour : MonoBehaviour {
 
+    public PlayerController playerControl;
+    public LevelManager.Levels thisLevelValue;
     public GameObject player;
     public GameObject mainCamera;
-
-    public void JumpToPlayer()
-    {
-        transform.position = new Vector3(mainCamera.transform.position.x, transform.position.y, transform.position.z);
-    }
+    public float distance;
+    public bool isStart = true;
+    public SpriteRenderer sprite1;
+    public SpriteRenderer sprite2;
 
     void Start()
     {
-        JumpToPlayer();
+        sprite1 = transform.FindChild("sprite1").GetComponent<SpriteRenderer>();
+        sprite2 = transform.FindChild("sprite2").GetComponent<SpriteRenderer>();
+        player = GameObject.Find("Player");
+        mainCamera = GameObject.Find("MainCamera");
+        playerControl = player.GetComponent<PlayerController>();
     }
 
     void Update()
     {
-        transform.position = new Vector3(mainCamera.transform.position.x * -5.0f, transform.position.y, transform.position.z);
+        if (playerControl.levelManager.currentLevel == thisLevelValue)
+        {
+            if (isStart)
+            {
+                sprite1.enabled = true;
+                sprite2.enabled = true;
+                transform.position = new Vector3(mainCamera.transform.position.x * (-1), transform.position.y, transform.position.z);
+                distance = mainCamera.transform.position.x - transform.position.x;
+                transform.position = new Vector3(distance, transform.position.y, transform.position.z);
+                isStart = false;
+            }
+            transform.position = new Vector3((distance - mainCamera.transform.position.x),transform.position.y,transform.position.z);
+        }
+        else
+        {
+            isStart = true;
+            sprite1.enabled = false;
+            sprite2.enabled = false;
+        }
     }
 }

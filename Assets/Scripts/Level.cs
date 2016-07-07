2248667e446d7da9476d9fbae53cdc36e048a269
Hulavanne <pyry.liljamo@dnainternet.net>
2016-07-07 13:16:09 +0300

    Revert "Boundaries now dynamically adjust to background size"
    This reverts commit a007a2d25095efea943825423af54904f53557aa.

--- Assets/Scripts/Level.cs
+++ Assets/Scripts/Level.cs
@@ -6,56 +6,12 @@ public class Level : MonoBehaviour
 	public bool fixedCamera = false;
 	public float levelLightAmount;
 
-    public GameObject playerBoundaries;
-    public GameObject cameraBoundaries;
-
-    GameObject boundaries;
+	LevelManager levelManager;
 
 	void Awake()
 	{
-        // Boundaries parent object
-        if (transform.FindChild("Boundaries") != null)
-        {
-            boundaries = transform.FindChild("Boundaries").gameObject;
-        }
-        else
-        {
-            boundaries = new GameObject("Boundaries");
-            boundaries.transform.parent = transform;
-        }
-
-        // Player boundaries
-        if (boundaries.transform.FindChild("PlayerBoundary") != null)
-        {
-            playerBoundaries = boundaries.transform.FindChild("PlayerBoundary").gameObject;
-        }
-        else
-        {
-            playerBoundaries = (GameObject)Instantiate(playerBoundaries, transform.position, transform.rotation);
-            playerBoundaries.transform.parent = boundaries.transform;
-        }
-
-        // Camera boundaries
-        if (boundaries.transform.FindChild("CameraBoundary") != null)
-        {
-            cameraBoundaries = boundaries.transform.FindChild("CameraBoundary").gameObject;
-        }
-        else
-        {
-            cameraBoundaries = (GameObject)Instantiate(cameraBoundaries, transform.position, transform.rotation);
-            cameraBoundaries.transform.parent = boundaries.transform;
-        }
-
-        if (transform.FindChild("Background").transform.FindChild("BackStatic") != null)
-        {
-            Sprite background = transform.FindChild("Background").transform.FindChild("BackStatic").GetComponent<SpriteRenderer>().sprite;
-
-            float leftX = transform.position.x - background.bounds.extents.x;
-            float rightX = transform.position.x + background.bounds.extents.x;
-
-            cameraBoundaries.transform.position = new Vector3(leftX, cameraBoundaries.transform.position.y, cameraBoundaries.transform.position.z);
-            cameraBoundaries.transform.GetChild(0).position = new Vector3(rightX, cameraBoundaries.transform.position.y, cameraBoundaries.transform.position.z);
-        }
+		levelManager = GameObject.Find("LevelManager").GetComponent<LevelManager>();
+		//levelManager.levels.Add(gameObject);
 	}
 
 	void Update()
